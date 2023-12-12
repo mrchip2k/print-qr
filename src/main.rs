568a62a50a867e6c25215927ea0219d22613f34d
mrchip2k <alemetzen3@gmail.com>
2023-12-12 14:17:39 +0100

    add stdin support

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,42 @@
 use qrcode::{render::unicode, QrCode};
 use std::env;
+use std::io::{self, BufRead};
 
 fn main() {
-    print(get_text());
+    let mut text = get_args_text();
+
+    if text.trim().is_empty() {
+        eprintln!("No arguments recieved, reading stdin.");
+        text = get_stdin_text();
+        if text.trim().is_empty() {
+            eprintln!("Error: No input was found.\nEither pass argument(s) to the program, or send something via stdin.");
+            std::process::exit(1);
+        }
+    }
+
+    print(text);
 }
 
-fn get_text() -> String {
+fn get_args_text() -> String {
     let mut args: Vec<String> = env::args().collect();
 
     args.remove(0);
 
-    if args.is_empty() {
-        println!("Error: No text provided!");
-        std::process::exit(1);
+    return args.join(" ");
+}
+
+fn get_stdin_text() -> String {
+    let mut result_string = String::new();
+
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        result_string.push_str(&line.unwrap().trim());
+        result_string.push_str("\n");
     }
 
-    return args.join(" ");
+    result_string = strip_ansi_escapes::strip_str(result_string);
+
+    return result_string.trim().to_string();
 }
 
 fn print(text: String) {
